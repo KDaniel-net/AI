b3502654fdd6d16dc6130bd96595a2715cedab3f
Daniel <peerato21@naver.com>
2023-01-07 23:42:54 +0900

    Train_test4

--- keras/keras08_Train_test4.py
+++ keras/keras08_Train_test4.py
@@ -10,14 +10,17 @@ y = np.array([[1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
              [1, 1, 1, 1, 2, 1.3, 1.4, 1.5, 1.6, 1.4]])
 # 실습:넘피아 리스트 슬라이싱// 7:3으로 잘라라!!!
 # train과 test를 섞어서 7:3을 만든다. 힌트: 사이킷 
-# 2. 모델
-model = Sequential()
-model.add(Dense(6,input_dim=3))
-model.add(Dense(7))
-model.add(Dense(2))
+x = x.T
+y = y.T
 
 x_train,x_test,y_train,y_test = train_test_split(x,y,
                                                  test_size=0.3,
                                                  shuffle=True,
+                                                 stratify=None,
                                                  random_state=123)
 
+print('x_train  ',x_train)
+print('x_test   ',x_test)
+
+print('y_train   ',y_train)
+print('y_tess    ',y_test)

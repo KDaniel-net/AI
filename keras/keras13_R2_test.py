08f96ec2a3f12622f89b2f26132e99574ae28a17
Daniel <peerato21@naver.com>
2023-01-08 11:11:07 +0900

    R2_test

--- keras/keras13_R2_test.py
+++ keras/keras13_R2_test.py
@@ -21,33 +21,63 @@ import numpy as np
 x = np.array(range(1, 21))
 y = np.array(range(21, 41))
 
+
 x_train, x_test, y_train, y_test = train_test_split(x,y,
                                                     train_size=0.7,
                                                     shuffle=True,
                                                     random_state=123)
 # 2.모델구성
 model = Sequential()
-model.add(Dense(6,input_dim=1))
+model.add(Dense(100,input_dim=1))
 model.add(Dense(10))
+model.add(Dense(100))
+model.add(Dense(10))
+model.add(Dense(100))
+model.add(Dense(10))
+model.add(Dense(100))
 model.add(Dense(10))
 model.add(Dense(100))
 model.add(Dense(10))
 model.add(Dense(10))
+model.add(Dense(100))
+model.add(Dense(10))
+model.add(Dense(100))
+model.add(Dense(10))
+model.add(Dense(100))
+model.add(Dense(10))
+model.add(Dense(100))
+model.add(Dense(10))
+model.add(Dense(100))
+model.add(Dense(10))
+model.add(Dense(100))
+model.add(Dense(10))
+model.add(Dense(100))
+model.add(Dense(10))
+model.add(Dense(100))
 model.add(Dense(10))
 model.add(Dense(10))
+model.add(Dense(100))
+model.add(Dense(10))
+model.add(Dense(100))
+model.add(Dense(10))
+model.add(Dense(100))
+model.add(Dense(10))
+model.add(Dense(100))
 model.add(Dense(1))
 
 # 3.컴파일
-model.compile(loss='mae',optimizer='adam',metrics=['mae'])
-model.fit(x_train,y_train,epochs=150,batch_size=1)
+model.compile(loss='mse',optimizer='adam',metrics=['mae'])
+model.fit(x_train,y_train,epochs=100,batch_size=1)
 
 # 4.평가,예측
 loss = model.evaluate(x_test,y_test)
 print('loss :' ,loss)
+
 y_predict = model.predict(x_test)
 
-def RMSE(y_test,y_predict):
+def RMSE(y_test,x_predict):
     return np.sqrt(mean_squared_error(y_test, y_predict))
+
 print('RMSE :' , RMSE(y_test, y_predict))
 
 r2 = r2_score(y_test, y_predict)

9f276d59a5710f59b97e26e5d04a3bde03543fee
jong <peerato21@naver.com>
2023-01-11 12:27:14 +0900

    all updata

--- keras/keras22_softmax2_wine.py
+++ keras/keras22_softmax2_wine.py
@@ -34,7 +34,7 @@ x_train,x_test,y_train,y_test = train_test_split(
 model = Sequential()
 model.add(Dense(5, activation='relu' , input_shape=(13,)))
 model.add(Dense(80))
-model.add(Dense(100))
+model.add(Dense(1))
 model.add(Dense(13))
 model.add(Dense(3,activation='softmax'))
 # 다중 분류에서는 softmax만 사용한다. 확률의 총합은 1, 대게 output층에서 사용한다. 
@@ -47,11 +47,11 @@ from tensorflow.keras.callbacks import EarlyStopping
 
 earlyStopping = EarlyStopping(monitor='val_loss',                # 무슨 값을 기준으로 할것인지
                              mode='max',                        # 최소값을 찾아준다. auto,max가 있음
-                             patience=10,                        # 몇번을 참아줄지/ 예를들어 5면 출력값에 뒤에서 5번째 앞에 값이 제일 크다.(함수에 따라 달라짐)
+                             patience=5,                        # 몇번을 참아줄지/ 예를들어 5면 출력값에 뒤에서 5번째 앞에 값이 제일 크다.(함수에 따라 달라짐)
                              restore_best_weights=True,
                              verbose=1)
 
-hist = model.fit(x_train,y_train,epochs=100,batch_size=1,   # 맨 앞에 붙은 hist는 결과 값. 즉, 내용을 history에 담겠다는 의미
+hist = model.fit(x_train,y_train,epochs=40,batch_size=2,   # 맨 앞에 붙은 hist는 결과 값. 즉, 내용을 history에 담겠다는 의미
           validation_split=0.2, # 0.2만큼 잘라서 validation값으로 사용한다.
           callbacks=[earlyStopping],
           verbose=2)
@@ -66,8 +66,11 @@ y_test = np.argmax(y_test, axis=1)
 print('y_test(원래값) :',y_test)
 
 acc = accuracy_score(y_test, y_predict) 
-print('acc :',acc)
+print('acc(정확도) :',acc)
 
 '''
 acc : 0.4166666666666667
+
+acc(정확도) : 0.6111111111111112
+
 '''

d92e0692816a898c31d3c694681fa2ea2ec2eb09
jong <peerato21@naver.com>
2023-01-13 18:26:35 +0900

    val_오류 수정

--- keras/keras34_2_cifar10.py
+++ keras/keras34_2_cifar10.py
@@ -17,18 +17,17 @@ from tensorflow.keras.layers import Conv2D, Dense, Flatten
 # 2. 모델
 model = Sequential()
 model.add(Conv2D(filters=128, kernel_size=(2,2), input_shape=(32,32,3), 
-                 activation='relu'))                    # (31,31,128)
-model.add(Conv2D(filters=64, kernel_size=(2,2)))         # (30,30,64)
-model.add(Conv2D(filters=64, kernel_size=(2,2)))         # (29,29,64) -> 53824
-model.add(Flatten())                                    # -> 53824
-model.add(Dense(32, activation='relu'))                 # input_shape = (53824,)
+                 activation='relu'))                    
+model.add(Conv2D(filters=64, kernel_size=(2,2)))         
+model.add(Conv2D(filters=64, kernel_size=(2,2)))         
+model.add(Flatten())                                   
+model.add(Dense(50, activation='relu'))                
 model.add(Dense(10, activation='softmax'))
 
 # 3.컴파일
 model.compile(loss='sparse_categorical_crossentropy', optimizer='adam',
-              metrics=['acc'])              # metrics에 acc가 적용이 되었기 때문에 results에는 2개의 값이 들어간다.
-
-model.fit(x_train, y_train, epochs=50, verbose=1, batch_size=100, validation_split=0.2)
+              metrics=['acc'])              
+model.fit(x_train, y_train, epochs=200, verbose=1, batch_size=100, validation_split=0.2)
 
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 es = EarlyStopping(monitor='val_loss', mode='min', patience=5 ,restore_best_weights=True, verbose=1)

c1bac3b05ed30150ba4f0ef60feba957fa2d3cbe
KDaniel-net <peerato21@naver.com>
2023-01-14 22:24:54 +0900

    예측값

--- new study/keras/keras02.py
+++ new study/keras/keras02.py
@@ -8,10 +8,7 @@ y = np.array([1,2,3,4,5,6,7,8,9,10])
 
 # 2.model
 model = Sequential()
-model.add(Dense(2,input_dim=1))
-model.add(Dense(10))
-model.add(Dense(5))
-model.add(Dense(1))
+model.add(Dense(1,input_dim=1))
 
 # 3.compile
 model.compile(loss='mae',optimizer='adam')
@@ -22,5 +19,5 @@ result = model.predict([13])
 print('결과 :' , result)
 
 ''' Epoch 5000/5000
-1/1 [==============================] - 0s 7ms/step - loss: 0.0011
-결과 : [[12.936455]] '''
+1/1 [==============================] - 0s 2ms/step - loss: 5.8052e-04
+결과 : [[13.001655]] '''
--- new study/keras/keras03.py
+++ new study/keras/keras03.py
@@ -8,20 +8,16 @@ y = np.array([1,2,3,5,4])
 
 # 2.model
 model = Sequential()
-model.add(Dense(800,input_dim=1))
-model.add(Dense(100))
-model.add(Dense(50))
-model.add(Dense(10))    
-model.add(Dense(1))    
+model.add(Dense(1,input_dim=1))
 
 # 3.compile
 model.compile(loss='mae',optimizer='adam')
-model.fit(x,y,epochs=100)
+model.fit(x,y,epochs=1000)
 
 # 4.prediction
 result = model.predict([16])
 print('결과 :',result)
 
-# Epoch 100/100
-# 1/1 [==============================] - 0s 4ms/step - loss: 0.4161
-# 결과 : [[15.256533]]
+''' Epoch 1000/1000
+1/1 [==============================] - 0s 3ms/step - loss: 0.4001
+결과 : [[15.998298]] '''
